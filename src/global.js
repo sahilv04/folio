03761bc31b532f6df01ba5e8ee36e46481392bbd
Sahil Verma <sahilv04@gmail.com>
2023-02-12 04:56:06 +0000

    change in package

--- src/global.js
+++ src/global.js
@@ -19,4 +19,8 @@ export const GlobalStyles = createGlobalStyle`
     // padding: 0;
     font-family: BlinkMacSystemFont, -apple-system, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif;
     transition: all 0.25s linear;
+  }
+  
+  div#root{
+    width: 100%;
   }`;
